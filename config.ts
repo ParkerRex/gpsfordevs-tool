218b64ced2978e0f23fdd1a6230985bb3b2f265c
Parker Rex <parker.m.rex@gmail.com>
2023-12-01 15:26:51 -0500

    cloudkit

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://developer.apple.com/documentation/swiftdata/",
-  match: "https://developer.apple.com/documentation/swiftdata/**",
+  url: "https://developer.apple.com/documentation/cloudkit/",
+  match: "https://developer.apple.com/documentation/cloudkit/**",
   selector: `.doc-content`,
-  maxPagesToCrawl: 1000,
-  outputFileName: "output.json",
+  maxPagesToCrawl: 1500,
+  outputFileName: "cloudkit.json",
 };
