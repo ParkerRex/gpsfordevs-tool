221b1689aa37bc0b3e89c0fc0d46c9527977bc90
Parker Rex <parker.m.rex@gmail.com>
2023-12-03 00:55:53 -0500

    feat: coreml

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://developer.apple.com/documentation/corehaptics",
-  match: "https://developer.apple.com/documentation/corehaptics/**",
+  url: "https://developer.apple.com/documentation/coreml",
+  match: "https://developer.apple.com/documentation/coreml/**",
   selector: `.doc-content`,
   maxPagesToCrawl: 25000,
-  outputFileName: "corehaptics.json",
+  outputFileName: "coreml.json",
 };
