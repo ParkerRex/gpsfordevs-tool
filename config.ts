12f8bb06c536b5c83a6785cd01294e41cf8feb40
Parker Rex <parker.m.rex@gmail.com>
2023-12-03 08:03:39 -0500

    mapkit

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://developer.apple.com/documentation/coreml",
-  match: "https://developer.apple.com/documentation/coreml/**",
+  url: "https://developer.apple.com/documentation/mapkit",
+  match: "https://developer.apple.com/documentation/mapkit/**",
   selector: `.doc-content`,
   maxPagesToCrawl: 25000,
-  outputFileName: "coreml.json",
+  outputFileName: "mapkit.json",
 };
