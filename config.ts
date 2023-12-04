baa254b6d4357c76024aa8ba202a1d9adba0d76b
Parker Rex <parker.m.rex@gmail.com>
2023-12-04 08:42:22 -0500

    added observation

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://developer.apple.com/documentation/foundation",
-  match: "https://developer.apple.com/documentation/foundation/**",
+  url: "https://developer.apple.com/documentation/avfoundation",
+  match: "https://developer.apple.com/documentation/avfoundation/**",
   selector: `.doc-content`,
   maxPagesToCrawl: 25000,
-  outputFileName: "foundation.json",
+  outputFileName: "observation.json",
 };
