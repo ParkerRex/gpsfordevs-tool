fbcee0e12f21ddb4b2c79a8966f6fa348947bef8
Parker Rex <parker.m.rex@gmail.com>
2023-12-12 09:38:55 -0500

    added coregraphics

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://developer.apple.com/documentation/uikit",
-  match: "https://developer.apple.com/documentation/uikit/**",
+  url: "https://developer.apple.com/documentation/coregraphics",
+  match: "https://developer.apple.com/documentation/coregraphics/**",
   selector: `.doc-content`,
   maxPagesToCrawl: 1000000,
-  outputFileName: "uikit.json",
+  outputFileName: "coregraphics.json",
 };
