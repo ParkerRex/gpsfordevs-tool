5721370110de6f7bab0936c1fb075758c7bb2249
Parker Rex <parker.m.rex@gmail.com>
2023-12-13 14:57:17 -0500

    widgetkit

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://developer.apple.com/documentation/Homekit",
-  match: "https://developer.apple.com/documentation/Homekit/**",
+  url: "https://developer.apple.com/documentation/WidgetKit",
+  match: "https://developer.apple.com/documentation/WidgetKit/**",
   selector: `.doc-content`,
   maxPagesToCrawl: 1000000,
-  outputFileName: "Homekit.json",
+  outputFileName: "WidgetKit.json",
 };
