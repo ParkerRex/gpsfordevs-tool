455fbeb86ad03bfb4bff48cd875df9cb181ce52d
Parker Rex <parker.m.rex@gmail.com>
2023-12-12 21:23:06 -0500

    feat: metal

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://developer.apple.com/documentation/accelerate",
-  match: "https://developer.apple.com/documentation/accelerate/**",
+  url: "https://developer.apple.com/documentation/ARKit",
+  match: "https://developer.apple.com/documentation/ARKit/**",
   selector: `.doc-content`,
   maxPagesToCrawl: 1000000,
-  outputFileName: "accelerate.json",
+  outputFileName: "ARKit.json",
 };
