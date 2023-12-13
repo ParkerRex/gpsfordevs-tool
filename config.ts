4ace93bfd90b995528fce24f37cdc49b4e9c2657
Parker Rex <parker.m.rex@gmail.com>
2023-12-13 12:57:28 -0500

    metalperformanceshaders

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://developer.apple.com/documentation/appstoreconnectapi",
-  match: "https://developer.apple.com/documentation/appstoreconnectapi/**",
+  url: "https://developer.apple.com/documentation/metalperformanceshaders",
+  match: "https://developer.apple.com/documentation/metalperformanceshaders/**",
   selector: `.doc-content`,
   maxPagesToCrawl: 1000000,
-  outputFileName: "appstoreconnect.json",
+  outputFileName: "metalperformanceshaders.json",
 };
