37d117bba7b11e31d29f1175b54bb57d8505ec93
Parker Rex <parker.m.rex@gmail.com>
2023-12-13 14:30:18 -0500

    homekit

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://developer.apple.com/documentation/metalperformanceshaders",
-  match: "https://developer.apple.com/documentation/metalperformanceshaders/**",
+  url: "https://developer.apple.com/documentation/Homekit",
+  match: "https://developer.apple.com/documentation/Homekit/**",
   selector: `.doc-content`,
   maxPagesToCrawl: 1000000,
-  outputFileName: "metalperformanceshaders.json",
+  outputFileName: "Homekit.json",
 };
