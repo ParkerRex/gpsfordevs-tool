075888b92f8d47cd3fa7f1577c5da25493651e84
Parker Rex <parker.m.rex@gmail.com>
2023-12-15 11:01:04 -0500

    extensionkiot

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://developer.apple.com/documentation/Swift",
-  match: "https://developer.apple.com/documentation/Swift/**",
+  url: "https://developer.apple.com/documentation/app_clips",
+  match: "https://developer.apple.com/documentation/app_clips/**",
   selector: `.doc-content`,
   maxPagesToCrawl: 10000000,
-  outputFileName: "Swift.json",
+  outputFileName: "app_clips.json",
 };
