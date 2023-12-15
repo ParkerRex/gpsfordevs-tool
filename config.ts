2d593675a310c6d7c4e2480b8e49b62aff6e52cf
Parker Rex <parker.m.rex@gmail.com>
2023-12-15 11:19:58 -0500

    appkit

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://developer.apple.com/documentation/app_clips",
-  match: "https://developer.apple.com/documentation/app_clips/**",
+  url: "https://developer.apple.com/documentation/appkit",
+  match: "https://developer.apple.com/documentation/appkit/**",
   selector: `.doc-content`,
   maxPagesToCrawl: 10000000,
-  outputFileName: "app_clips.json",
+  outputFileName: "appkit.json",
 };
