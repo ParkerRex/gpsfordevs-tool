56d52eb50bd2b1dff3819f3f608e920c288b96fe
Parker Rex <parker.m.rex@gmail.com>
2023-12-16 11:43:13 -0500

    objc

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://developer.apple.com/documentation/appkit",
-  match: "https://developer.apple.com/documentation/appkit/**",
+  url: "https://developer.apple.com/documentation/objectivec",
+  match: "https://developer.apple.com/documentation/objectivec/**",
   selector: `.doc-content`,
   maxPagesToCrawl: 10000000,
-  outputFileName: "appkit.json",
+  outputFileName: "objectivec.json",
 };
